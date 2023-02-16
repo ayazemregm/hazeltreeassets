306e9edf7616a75bde5630d9e61fd8c47d97e1eb
gmtolgatopkac <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-16 19:36:07 +0300

    Update skilljar-init-hazeltree.js

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -4,26 +4,26 @@ let sourceLink = "https://ayazemregm.github.io/hazeltreeassets/";
 console.log("loaded");
 //   defaultActions();
 
-switch (end) {
-  case "/":
-    console.log("Main Page");
-    break;
-  case "/page/hazeltree-university":
-    console.log("testPage");
-    let css = `<link rel="stylesheet" href="${sourceLink}catalog-hazeltree.css">
+window.addEventListener("load", () => {
+  switch (end) {
+    case "/":
+      console.log("Main Page");
+      break;
+    case "/page/hazeltree-university":
+      console.log("testPage");
+      let css = `<link rel="stylesheet" href="${sourceLink}catalog-hazeltree.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>`;
-    let script = `<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+      let script = `<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <script src="${sourceLink}catalog-page.js"></script>`;
-    document.head.insertAdjacentHTML("beforeend", css);
-    document.head.insertAdjacentHTML("beforeend", script);
+      document.head.insertAdjacentHTML("beforeend", css);
+      document.body.insertAdjacentHTML("afterend", script);
 
-    break;
-  default:
-    console.log("Default Fired");
-    break;
-}
-
-// window.addEventListener("load", () => {});
+      break;
+    default:
+      console.log("Default Fired");
+      break;
+  }
+});
 
 function defaultActions() {
   let footer = document.getElementById("ep-footer");
