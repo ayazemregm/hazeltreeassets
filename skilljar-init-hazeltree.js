5e3a4783d4a17bc165d75342bba61d5dcf8b51c2
gmtolgatopkac <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-16 19:10:40 +0300

    Update skilljar-init-hazeltree.js

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
       let template = `<link rel="stylesheet" href="${sourceLink}catalog-hazeltree.css">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
       <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-       <script defer src="${sourceLink}catalog-page.js></script>
+       <script defer src="${sourceLink}catalog-page.js"></script>
       `;
 
       document.head.insertAdjacentHTML("beforeend", template);
