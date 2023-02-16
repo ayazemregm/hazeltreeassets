bc818a7f948fc9fc21bdeb814bf9390b4b509130
gmtolgatopkac <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-16 22:13:48 +0300

    Update skilljar-init-hazeltree.js

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -87,19 +87,16 @@ function initializeSwiper() {
     },
     slidesPerView: 3,
     paginationClickable: false,
-    spaceBetween: 20,
+
     breakpoints: {
       1920: {
         slidesPerView: 3,
-        spaceBetween: 30,
       },
       1028: {
         slidesPerView: 2,
-        spaceBetween: 30,
       },
       480: {
         slidesPerView: 1,
-        spaceBetween: 10,
       },
     },
   });
