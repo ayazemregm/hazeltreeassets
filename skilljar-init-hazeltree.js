cd3eca764a96235aad7e83587dfd54ad415745b5
gmtolgatopkac <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-16 20:45:27 +0300

    Update skilljar-init-hazeltree.js

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -102,8 +102,10 @@ function initializeSwiper() {
   });
 
   let catalogCourses = document.getElementById("catalog-courses");
+  let sliders = document.getElementById("sliders");
   for (const child of catalogContent.children) {
     let swiperSlide = `<div class="swiper-slide md:md:max-w-lg space-y-6 ">${child}</div>`;
+    sliders.insertAdjacentHTML("afterbegin", swiperSlide);
   }
 
   catalogCourses.remove();
