3840d9d85a3438a592907c687b9f307ace1c9e97
gmtolgatopkac <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-17 11:23:25 +0300

    Update skilljar-init-hazeltree.js

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -173,6 +173,70 @@ tab-4
       item.classList.add("active");
     });
   });
+
+  document.getElementById("asset-managers").innerHTML = `  
+<!-- Slider main container -->
+<div class="swiper swiper1">
+  <p class="mt-12 mb-10 font-bold text-lg md:text-2xl px-4 md:px-0">Assets Managers</p>
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper ">
+    <!-- Slides -->
+      <div class="swiper-slide md:md:max-w-lg space-y-6 ">
+        <div>
+          <img class="w-[360px] h-[250px]" src="img/slider1.png" alt="">
+        </div>
+            <div class="space-y-4 max-w-xs">
+              <p class="font-medium text-lg">Lorem Ipsum</p>
+              <p class="font-light leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                libero et velit interdum, ac
+                aliquet odio
+                mattis.</p>
+            </div>
+          </div>
+                     <div class="swiper-slide md:max-w-lg space-y-6 ">
+            <div>
+              <img class="w-[360px] h-[250px]" src="img/slider1.png" alt="">
+            </div>
+            <div class="space-y-4 max-w-xs">
+              <p class="font-medium text-lg">Lorem Ipsum</p>
+              <p class="font-light leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                libero et velit interdum, ac
+                aliquet odio
+                mattis.</p>
+            </div>
+          </div>
+            <div class="swiper-slide md:max-w-lg space-y-6 ">
+            <div>
+              <img class="w-[360px] h-[250px]" src="img/slider1.png" alt="">
+            </div>
+            <div class="space-y-4 max-w-xs">
+              <p class="font-medium text-lg">Lorem Ipsum</p>
+              <p class="font-light leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                libero et velit interdum, ac
+                aliquet odio
+                mattis.</p>
+            </div>
+          </div>
+
+                      <div class="swiper-slide md:max-w-lg space-y-6 ">
+            <div>
+              <img class="w-[360px] h-[250px]" src="img/slider1.png" alt="">
+            </div>
+            <div class="space-y-4 max-w-xs">
+              <p class="font-medium text-lg">Lorem Ipsum</p>
+              <p class="font-light leading-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                libero et velit interdum, ac
+                aliquet odio
+                mattis.</p>
+            </div>
+          </div>
+  </div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev "></div>
+  <div class="swiper-button-next"></div>
+
+</div>`;
 }
 // footer
 function footer() {
