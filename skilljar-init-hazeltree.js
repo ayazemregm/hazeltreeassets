78db85a4c43cf503a74746e5e724ad028b83845a
gmtolgatopkac <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-17 10:08:53 +0300

    Update skilljar-init-hazeltree.js

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -117,7 +117,7 @@ function tabs() {
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
     </div>`;
   let catalogContent = document.querySelector("#catalog-content");
-  catalogContent.insertAdjacentHTML = ("afterend", tabsText);
+  catalogContent.insertAdjacentHTML = ("afterbegin", tabsText);
   console.log(tabsText);
 }
 // footer
