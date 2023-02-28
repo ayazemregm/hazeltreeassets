384b0d5cb3ddef6213c01ce07c7800e5e35ffa39
tolgatopkac-gm <119890246+tolgatopkac-gm@users.noreply.github.com>
2023-02-28 14:54:58 +0300

    up

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -256,7 +256,7 @@ function tabs() {
 
             let cloneNode = dataCourse[0].cloneNode(true);
             // cloneNode.item(1).data - src;
-            console.log(cloneNode.item);
+            console.log(cloneNode.item(1));
             courseNodeEl.appendChild(cloneNode);
             /*   document.querySelector(
               `[data-course='${courseNode.dataset.course}']`
