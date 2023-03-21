c01ca1fb6dd62519e9c0018e18e2f21105dbc261
Emre Ayaz <emre@greenmusk.com>
2023-03-21 14:46:11 +0300

    oninput

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -62,7 +62,7 @@ window.addEventListener("load", () => {
   console.log(searchBox);
   if (searchBox) {
     console.log("event listener added");
-    searchBox.addEventListener("onchange", () => {
+    searchBox.addEventListener("oninput", () => {
       console.log("search working");
     });
   }
