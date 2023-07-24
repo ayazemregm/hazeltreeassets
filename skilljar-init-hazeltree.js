530ddce30792bfeedd7dfe7dad67268334c2d61a
Emre Ayaz <emre@greenmusk.com>
2023-07-24 17:44:22 +0300

    revert

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -312,7 +312,10 @@ function tabs() {
     if (i === 1) {
       elBtnTemplate = `<div class="swiper-slide"><button id="pathTab1" class="tab-nav-item tab-nav-item-active">${tab}</button></div>`;
     }
-
+    tabsNav.insertAdjacentHTML("beforeend", elBtnTemplate);
+    document
+      .getElementById("tabs-content")
+      .insertAdjacentHTML("afterbegin", tabTemplate);
 
     let axiosUrl = `${filteredPaths[i - 1].href}`;
     // console.log(filteredPaths);
@@ -325,10 +328,6 @@ function tabs() {
         console.log(pathCourses);
 
         if (pathCourses && pathCourses.children !== null && pathCourses.children.length > 0) {
-          tabsNav.insertAdjacentHTML("beforeend", elBtnTemplate);
-          document
-            .getElementById("tabs-content")
-            .insertAdjacentHTML("afterbegin", tabTemplate);
 
           for (let k = 0; k < pathCourses.children.length; k++) {
             let tabSliders = document.getElementById(`tab-sliders${i}`);
