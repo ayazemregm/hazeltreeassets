2d5f6946c37063579cec9ea4d0aacd532b5562c7
Emre Ayaz <emre@greenmusk.com>
2023-08-16 18:35:30 +0300

    pathspaginationfix

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -278,10 +278,10 @@ function tabs() {
         <div id="tabs-title">
             <div class="tabs-nav">
                 <div class="swiper-container swiper-tabs-container">
+                <div class="swiper-tabs swiper-all">
+                <div id="paths-sliders" class="swiper-wrapper" style="align-items: end;"></div>
+                </div>
                 <div class="swiper-pagination-paths"></div>
-                    <div class="swiper-tabs swiper-all">
-                        <div id="paths-sliders" class="swiper-wrapper" style="align-items: end;"></div>
-                    </div>
                     <div class="swiper-button-prev-paths swiper-button-prev swiper-button-prev-all"></div>
                     <div class="swiper-button-next-paths swiper-button-next swiper-button-next-all"></div>
                 </div>
