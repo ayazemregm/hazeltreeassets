c65bbb91488744fc2374cabd770570245fd86b3f
Emre Ayaz <emre@greenmusk.com>
2023-08-16 14:38:44 +0300

    log5

--- skilljar-init-hazeltree.js
+++ skilljar-init-hazeltree.js
@@ -333,7 +333,7 @@ function tabs() {
         elBtnTemplate.innerHTML = `<button id="pathTab1" class="tab-nav-item tab-nav-item-active">${tab}</button>`;
       }
       if (filteredPaths[i - 1].children.item(5).innerText !== "0 Courses") {
-        console.log(filteredPaths[i - 1].children.item(5).innerText);
+        console.log(filteredPaths[i - 1].children.item(5).innerText.trim());
       }
       tabsNav.insertBefore(elBtnTemplate, tabsNav.children[i - 1]);
       document
